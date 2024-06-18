581864bfd98fada99814b42fd726f80f0c4de5b5
Unit111 <unit_1@abv.bg>
2024-06-18 18:59:10 +0300

    new stuff

--- example_package/main.py
+++ example_package/main.py
@@ -4,3 +4,7 @@ import string
 
 def random_string(length: int) -> str:
     return ''.join(random.choice(string.ascii_letters) for _ in range(length))
+
+
+def random_number(max_number: int) -> int:
+    return random.randint(0, max_number)
